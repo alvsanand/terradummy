7d1b59b6d490775314b1f024ae2741e38f82e627
Alvaro Santos Andres <alvaro.santos@bluetab.net>
2019-11-28 11:27:59 +0100

    Added solution for step-1

--- main.tf
+++ main.tf
@@ -10,9 +10,20 @@ terraform {
 # - owner-alias == amazon
 # - virtualization-type == hvm
 # - owners == hvm
-
 data "aws_ami" "amazon_linux_ami" {
-  # TODO
+  most_recent = true
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
 }
 
 # Create an aws instance that
@@ -20,5 +31,10 @@ data "aws_ami" "amazon_linux_ami" {
 # - instance_type == t2.micro
 # - tag Name == DummyMachine
 resource "aws_instance" "web" {
-  # TODO
+  ami           = "${data.aws_ami.amazon_linux_ami.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DummyMachine"
+  }
 }
