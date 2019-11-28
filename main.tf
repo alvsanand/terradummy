481d492a26d3b80b639dee3a8ada3d8794633ae5
Alvaro Santos Andres <alvaro.santos@bluetab.net>
2019-11-28 11:20:30 +0100

    Removed solutions

--- main.tf
+++ main.tf
@@ -7,29 +7,19 @@ provider "aws" {
   }
 }
 
-# Look for the AMI to inject to aws_instance
+# Look for an the "latest" AMI that:
+# - owner-alias == amazon
+# - virtualization-type == hvm
+# - owners == hvm
+
 data "aws_ami" "amazon_linux_ami" {
-  most_recent = true
-
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["amazon"]
+  # TODO
 }
 
-# Use the previous AMI and launch an EC2 instance
+# Create an aws instance that
+# - use the previous ami id
+# - instance_type == t2.micro
+# - tag Name == DummyMachine
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.amazon_linux_ami.id}"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "DummyMachine"
-  }
+    # TODO
 }
